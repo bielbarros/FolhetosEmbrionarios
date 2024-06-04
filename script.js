e8968b2b7b4739c86d8ef09fb932ecc4f11d1ef1
bielbarros <gabrielbarrosg11@gmail.com>
2024-06-03 21:51:53 -0300

    Atualização js e background

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 window.addEventListener('resize', function() {
-    document.body.style.height = window.innerHeight + 'px';
-  });
-  
+  document.body.style.height = window.innerHeight + 'px';
+  document.querySelector('.background-mobile').style.height = window.innerHeight + 'px';
+});
