7004a1ae67338df5673fd3288a6e9ca50b42cb12
bielbarros <gabrielbarrosg11@gmail.com>
2024-06-03 22:41:45 -0300

    RETIRAR JS

--- script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('resize', function() {
-  document.body.style.height = window.innerHeight + 'px';
-  document.querySelector('.background-mobile').style.height = window.innerHeight + 'px';
-});
